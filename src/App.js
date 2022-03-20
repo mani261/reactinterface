1dc13526e7b6e828eadecf955be5c175bad5f7e4
MaNi <mani.razavizadeh@gmail.com>
2022-03-20 15:16:45 +0330

    use hooks to fetch data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import { useState, useEffect, useCallback } from 'react';
 import './App.css';
 import { BiArchive } from 'react-icons/bi'
 import SearchFeild from './components/SearchFeild';
 import AddAppointment from './components/AddAppointment';
 import AppointmentInfo from './components/AppointmentInfo';
-import appDataList from './data.json';
 
 function App() {
+
+  let [appointmentList, setAppointmentList] = useState([])
+
+  const fetchData = useCallback(() => {
+    fetch('./data.json')
+    .then(response => response.json())
+    .then(data => setAppointmentList(data))
+  }, [])
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
   return (
     <div className="App p-5">
       <header className="App-header">
-        
+
         <h1 className='text-3xl font-bold py-8 text-center'><BiArchive className='text-red-400 inline-block mx-1' />Your Appointment</h1>
 
         <AddAppointment />
@@ -18,7 +31,7 @@ function App() {
       </header>
 
       <ul className='divide-y divide-gray-200 m-3'>
-        {appDataList.map(appointment => (
+        {appointmentList.map(appointment => (
           <AppointmentInfo key={appointment.id} appointment={appointment} />
         ))}
       </ul>
